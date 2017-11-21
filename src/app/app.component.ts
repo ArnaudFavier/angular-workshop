82fbfd15934e6f18f41b4bf7073ed634634c6490
Arnaud Favier <ArnaudFavier@users.noreply.github.com>
2017-11-21 10:05:11 +0100

    [ADD] 5 exercices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  name : string = '🙂';
+  users : string[] = ['Arnaud','Hey'];
+
+  sayHello(): void {
+    alert('Hello');
+  }
 }
